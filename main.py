96938b39cbb2bf9ecfc704fe8d63be1d603c7afc
dmitry <ikosha-tam@yandex.ru>
2022-04-25 12:29:04 +0300

    Update main.py, add my_module.py

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
-"""
-history = []
-
-def purchase(budget):
-    sum = int(input('Введите стоимость покупки: '))
-    if sum > budget:
-        print('Недостаточно средств')
-    else:
-        budget -= sum
-        name = input('Введите название покупки: ')
-        history.append((name, sum))
-    return budget
-
-
-    print(f'Ваш счет: {budget}')
-
-
-    sum = int(input('Введите сумму для пополнения: '))
-    budget += sum
-
-    sum = int(input('Введите сумму для пополнения: '))
-    budget += sum
-
-    budget = purchase(budget)
-
-    for item, cost in history:
-        print(f'{item}:\t {cost} руб.')
-    print(history)
-
-    print('Текущая папка: ', os.getcwd())
-    print(os.listdir())
-"""
 import shutil
 import sys
 import os
 import getpass
-
-budget = 0
+import my_module
 
 while True:
     print('-' * 100)
-    print('1. Создать папку')  # Сделано
-    print('2. Удалить (файл/папку)')  # Сделано
-    print('3. Копировать (файл/папку)')  # Сделано
-    print('4. Просмотр содержимого рабочей директории')  # Сделано
-    print('5. Посмотреть только папки')  # Сделано
-    print('6. Посмотреть только файлы')  # Сделано
-    print('7. Просмотр информации об операционной системе')  # Сделано
-    print('8. Создатель программы')  # Сделано
+    print('1. Создать папку')
+    print('2. Удалить (файл/папку)')
+    print('3. Копировать (файл/папку)')
+    print('4. Просмотр содержимого рабочей директории')
+    print('5. Посмотреть только папки')
+    print('6. Посмотреть только файлы')
+    print('7. Просмотр информации об операционной системе')
+    print('8. Создатель программы')
     print('9. Играть в викторину')
     print('10. Мой банковский счет')
     print('11. Смена рабочей директории (*необязательный пункт)')
     print('12. Выход')
     choice = input('Выберите пункт меню: ')
-
     if choice == '1':  # Создать папку
         name_dir = input('Имя новой папки: ')
         if not os.path.exists(name_dir):
@@ -101,11 +67,14 @@ while True:
     elif choice == '8':  # Создатель программы
         print(getpass.getuser())
     elif choice == '9':  # Играть в викторину
-        print(choice)
+        my_module.victory()
     elif choice == '10':  # Мой банковский счет
-        print(choice)
+        my_module.my_bank_account()
     elif choice == '11':  # Смена рабочей директории (*необязательный пункт)
-        print(choice)
+        print("Текущая директория:", os.getcwd())
+        p = os.path.normpath(os.path.join("/home",getpass.getuser()))
+        os.chdir(p)
+        print("Переход в домашнюю папку в Linux):", os.getcwd())
     elif choice == '12':  # Выход
         break
     else:
